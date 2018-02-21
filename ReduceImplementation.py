70fbfb72f98c3d5ffe94ba21ac964ffb21762339
mehmet özcan <mehmetozcan1994@hotmail.com>
2018-02-21 11:17:52 +0300

    Create ReduceImplementation.py

---
+++ ReduceImplementation.py
@@ -0,0 +1,10 @@
+def ReduceImplementation(X,params): 
+    params=list(params)
+    if len(params)>=2:
+        params[1]=params[0]=X(params[0],params[1])
+        return ReduceImplementation(X,params[1:])
+    else: 
+        return params[0]  
+ReduceImplementation(lambda x,y:x+y,[1,2,3,5])
+
+functools.reduce(lambda x,y:x+y,[1,2,3])
